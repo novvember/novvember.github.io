4614ab6336220c13d0166220615d79d905973c94
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-10-02 18:24:31 +0300

    feat: add my stack description and base styles

--- src/components/HelloMessage/HelloMessage.js
+++ src/components/HelloMessage/HelloMessage.js
@@ -19,11 +19,58 @@ function HelloMessage() {
           Привет! Я — Дмитрий. Фронтенд-разработчик (сейчас) и инженер-технолог
           (раньше).
         </p>
-        <p>
-          Этот блок еще не доделан :( Здесь будет указан мой стек, ссылки на
-          резюме, актуальная информация и небольшая навигация по трем разделам
-          этой страницы.
+
+        <div className="hello-message__stack">
+          <h2 className="hello-message__stack-title">Стек</h2>
+          <ul>
+            <li>
+              Основной стек: <span>HTML5</span> <span>CSS3</span>{' '}
+              <span>БЭМ</span> <span>JavaScript</span> <span>React.js</span>
+            </li>
+
+            <li>
+              Есть практика работы:
+              <ul>
+                <li>
+                  JavaScript: <span>ES6</span> <span>ООП</span>{' '}
+                  <span>async/await</span> <span>Swiper.js</span>
+                </li>
+                <li>
+                  React: <span>Create React App</span> <span>React Router</span>{' '}
+                  <span>React Context</span> <span>Ant Design</span>
+                </li>
+                <li>
+                  бэкенд: <span>Node.js</span> <span>Express.js</span>{' '}
+                  <span>Nginx</span> <span>Celebrate + Joi</span>{' '}
+                  <span>MongoDB + Mongoose</span>
+                </li>
+                <li>
+                  юнит-тестирование: <span>Jest</span> <span>supertest</span>
+                </li>
+                <li>
+                  общие инструменты: <span>Git</span> <span>npm</span>{' '}
+                  <span>bash</span> <span>Webpack</span> <span>Figma</span>{' '}
+                  <span>VS Code</span> <span>Postman</span> <span>ESLint</span>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              Хочу дополнительно изучить: <span>TypeScript</span>{' '}
+              <span>LESS/SASS</span> <span>Redux</span>{' '}
+              <span>React (углубленно)</span>
+            </li>
+          </ul>
+        </div>
+        <p className="hello-message__cv">
+          Ищу работу, в Москве или удаленно! Мое резюме: тут (+pdf), на
+          Хэдхантере, на Хабр.Карьере.
+          <p>
+            Закончил Яндекс.Практикум (веб-разработчик) + самостоятельное
+            обучение.
+          </p>
         </p>
+        <p className="hello-message__nav">Сделал, учусь, хочу сделать</p>
       </div>
     </section>
   );
